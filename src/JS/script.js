bb2450f830d069c011b5b6b6d14b48b40bdceba4
Ivan Reis <109477902+Ivan-ReisDev@users.noreply.github.com>
2022-10-04 15:33:19 -0300

    Add files via upload

---
+++ src/JS/script.js
@@ -0,0 +1,20 @@
+
+const fotos = [
+'./src/img/copo1.png',
+'./src/img/copo2.png',
+'./src/img/copo3.png',
+'./src/img/Starbucks-Coffee-Transparent-Free-PNG.png'
+]
+
+const trocarFotos = () => {
+  document.getElementById('foto').src=fotos[0];
+}
+const trocarFotos0 = () => {
+  document.getElementById('foto').src=fotos[1];
+}
+const trocarFotos1 = () => {
+  document.getElementById('foto').src=fotos[2];
+}
+const trocarFotos2 = () => {
+  document.getElementById('foto').src=fotos[3];
+}
